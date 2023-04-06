7dd14fe060ea97244092382faff4ac6c293881e2
Ruslan <foxhand7@gmail.com>
2023-04-06 17:30:07 +0300

    Install-Updates дописал
    Которая проверяет наличие доступных обновлений для установки и устанавливает их, если они есть. Если обновления требуют перезагрузки компьютера, функция произведет перезагрузку. Если функция не запущена с правами администратора, она выведет сообщение об ошибке.

--- UpdateApps.ps1
+++ UpdateApps.ps1
@@ -1,4 +1,4 @@
-﻿    [CmdletBinding()]
+    [CmdletBinding()]
     Param(
         [switch]$CheckInternetConnection,
         [switch]$CheckAdminRights,
@@ -38,34 +38,48 @@
         return $true
     }
 
-    function Install-Updates {
-        Write-Output 'Поиск доступных обновлений...'
-        $updates = (New-Object -ComObject Microsoft.Update.Session).CreateUpdateSearcher().Search("IsInstalled=0")
+    # Функция, которая проверяет, запущен ли сценарий с повышенными правами
+    function Test-Admin {
+        $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+        $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    }
 
-        # Проверка наличия обновлений
-        if ($updates.Updates.Count -eq 0) {
-            Log-Message "Нет доступных обновлений."
-            Write-Host "Нет доступных обновлений."
-        } else {
-            Log-Message "Найдено $($updates.Updates.Count) обновлений."
-            $updates.Updates | foreach {
-                $_.AcceptEula()
-                Log-Message "Установка обновления $($_.Title)..."
-                (New-Object -ComObject Microsoft.Update.UpdateInstaller).Install($updates)
+    # Функция, которая устанавливает обновления
+    function Install-Updates {
+        if (Test-Admin) {
+            Write-Host "Запущено с правами администратора. Проверяем наличие обновлений..."
+            $session = New-Object -ComObject Microsoft.Update.Session
+            $searcher = $session.CreateUpdateSearcher()
+            $updates = $searcher.Search("IsInstalled=0")
+            if ($updates.Updates.Count -eq 0) {
+                Write-Host "Нет доступных обновлений для установки."
+                return
             }
-            Log-Message "Все обновления установлены."
-            Write-Host "Все обновления установлены."
-            # Проверка успешности выполнения обновлений
-            $success = $updates.Updates | Where-Object {$_.IsInstalled -eq $false}
-            if ($success.Count -eq 0) {
-                Log-Message "Все обновления установлены успешно."
-                Write-Host "Все обновления установлены успешно."
+            Write-Host "Найдено $($updates.Updates.Count) обновлений для установки."
+            Write-Host "Начинаем загрузку обновлений..."
+            $updatesToInstall = New-Object -ComObject Microsoft.Update.UpdateColl
+            foreach ($update in $updates.Updates) {
+                $updatesToInstall.Add($update)
+            }
+            $downloader = $session.CreateUpdateDownloader()
+            $downloader.Updates = $updatesToInstall
+            $downloader.Download()
+            Write-Host "Обновления загружены. Начинаем установку..."
+            $installer = $session.CreateUpdateInstaller()
+            $installer.Updates = $updatesToInstall
+            $installationResult = $installer.Install()
+            if ($installationResult.ResultCode -eq "2") {
+                Write-Host "Установка обновлений потребует перезагрузки. Перезагружаем компьютер..."
+                Restart-Computer -Force
             } else {
-                Log-Message "Не все обновления были установлены."
-                Write-Error "Не все обновления были установлены."
+                Write-Host "Установка обновлений завершена."
             }
+        } else {
+            Write-Host "Для установки обновлений требуются права администратора."
         }
     }
+
+
 
     $LogPath = 'D:\System\Script\UpdateScript.log'
 
